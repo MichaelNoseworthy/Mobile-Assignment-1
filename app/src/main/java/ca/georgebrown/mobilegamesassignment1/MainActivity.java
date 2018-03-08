fd46121ce95d840d8136c2f13538a93e5d3c15db
MikeTheNose <michael.noseworthy@georgebrown.ca>
2018-03-07 20:16:26 -0500

    Setup Base Functions and variable initializations.

--- app/src/main/java/ca/georgebrown/mobilegamesassignment1/MainActivity.java
+++ app/src/main/java/ca/georgebrown/mobilegamesassignment1/MainActivity.java
@@ -84,6 +84,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     //stats
     long lastFrameTime;
     int fps;
@@ -191,8 +193,252 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        //Slot class:
+
+        public class PlaySlotGame{
+
+            int value = 0; //assuming these are ints during int
+            int lowerBounds = 0; //assuming these are ints during int
+            int upperBounds = 0; //assuming these are ints during int
+
+
+            // Utility function to show Player Stats
+
+            public void showPlayerStats(){
+                winRatio = winNumber / turn;
+                /*
+                $("#jackpot").text("Jackpot: " + jackpot);
+                $("#playerMoney").text("Player Money: " + playerMoney);
+                $("#playerTurn").text("Turn: " + turn);
+                $("#playerWins").text("Wins: " + winNumber);
+                $("#playerLosses").text("Losses: " + lossNumber);
+                $("#playerWinRatio").text("Win Ratio: " + (winRatio * 100).toFixed(2) + "%");
+                */
+            }
+
+
+// Utility function to reset all fruit tallies
+
+            void resetFruitTally() {
+                grapes = 0;
+                bananas = 0;
+                oranges = 0;
+                cherries = 0;
+                bars = 0;
+                bells = 0;
+                sevens = 0;
+                blanks = 0;
+            }
+
+// Utility function to reset the player stats
+            void resetAll() {
+                playerMoney = 1000;
+                winnings = 0;
+                jackpot = 5000;
+                turn = 0;
+                playerBet = 0;
+                winNumber = 0;
+                lossNumber = 0;
+                winRatio = 0;
+            }
+
+
+// Check to see if the player won the jackpot
+            void checkJackPot() {
+    // compare two random values
+                double jackPotTry = Math.floor(Math.random() * 51 + 1);
+                double jackPotWin = Math.floor(Math.random() * 51 + 1);
+                if (jackPotTry == jackPotWin) {
+                    //alert("You Won the $" + jackpot + " Jackpot!!"); //Call to paint!
+                    playerMoney += jackpot;
+                    jackpot = 1000;
+                }
+            }
+
+
+// Utility function to show a win message and increase player money
+            void showWinMessage() {
+                playerMoney += winnings;
+                //$("div#winOrLose>p").text("You Won: $" + winnings); //Call to paint!
+                resetFruitTally();
+                checkJackPot();
+            }
+
+
+// Utility function to show a loss message and reduce player money
+            void showLossMessage() {
+                playerMoney -= playerBet;
+                //$("div#winOrLose>p").text("You Lost!"); //Call to paint!
+                resetFruitTally();
+            }
+
+
+// Utility function to check if a value falls within a range of bounds
+            int checkRange(int value, int lowerBounds, int upperBounds) { //assuming these are ints during init
+                if (value >= lowerBounds && value <= upperBounds)
+                {
+                    return value;
+                }
+                else {
+                    //return !value;  // Converted from javascript...might be wrong.
+                    return 0;
+                }
+            }
+
+
+// When this function is called it determines the betLine results.
+//e.g. Bar - Orange - Banana
+            void Reels() { //Really need to rework this code.  Leaving out for now.
+                //string betLine = [" ", " ", " "]; //something to figure out
+                /*
+                int outCome[] = [0, 0, 0];
+
+                for (int spin = 0; spin < 3; spin++) {
+                    outCome[spin] = Math.floor((Math.random() * 65) + 1);
+                    switch (outCome[spin]) {
+                        case checkRange(outCome[spin], 1, 27):  // 41.5% probability
+                            betLine[spin] = "blank";
+                            blanks++;
+                            break;
+                        case checkRange(outCome[spin], 28, 37): // 15.4% probability
+                            betLine[spin] = "Grapes";
+                            grapes++;
+                            break;
+                        case checkRange(outCome[spin], 38, 46): // 13.8% probability
+                            betLine[spin] = "Banana";
+                            bananas++;
+                            break;
+                        case checkRange(outCome[spin], 47, 54): // 12.3% probability
+                            betLine[spin] = "Orange";
+                            oranges++;
+                            break;
+                        case checkRange(outCome[spin], 55, 59): //  7.7% probability
+                            betLine[spin] = "Cherry";
+                            cherries++;
+                            break;
+                        case checkRange(outCome[spin], 60, 62): //  4.6% probability
+                            betLine[spin] = "Bar";
+                            bars++;
+                            break;
+                        case checkRange(outCome[spin], 63, 64): //  3.1% probability
+                            betLine[spin] = "Bell";
+                            bells++;
+                            break;
+                        case checkRange(outCome[spin], 65, 65): //  1.5% probability
+                            betLine[spin] = "Seven";
+                            sevens++;
+                            break;
+                    }
+                }
+                return betLine;
+                */
+            }
+
+// This function calculates the player's winnings, if any
+            void determineWinnings()
+            {
+                if (blanks == 0)
+                {
+                    if (grapes == 3) {
+                        winnings = playerBet * 10;
+                    }
+                    else if(bananas == 3) {
+                        winnings = playerBet * 20;
+                    }
+                    else if (oranges == 3) {
+                        winnings = playerBet * 30;
+                    }
+                    else if (cherries == 3) {
+                        winnings = playerBet * 40;
+                    }
+                    else if (bars == 3) {
+                        winnings = playerBet * 50;
+                    }
+                    else if (bells == 3) {
+                        winnings = playerBet * 75;
+                    }
+                    else if (sevens == 3) {
+                        winnings = playerBet * 100;
+                    }
+                    else if (grapes == 2) {
+                        winnings = playerBet * 2;
+                    }
+                    else if (bananas == 2) {
+                        winnings = playerBet * 2;
+                    }
+                    else if (oranges == 2) {
+                        winnings = playerBet * 3;
+                    }
+                    else if (cherries == 2) {
+                        winnings = playerBet * 4;
+                    }
+                    else if (bars == 2) {
+                        winnings = playerBet * 5;
+                    }
+                    else if (bells == 2) {
+                        winnings = playerBet * 10;
+                    }
+                    else if (sevens == 2) {
+                        winnings = playerBet * 20;
+                    }
+                    else if (sevens == 1) {
+                        winnings = playerBet * 5;
+                    }
+                    else {
+                        winnings = playerBet * 1;
+                    }
+                    winNumber++;
+                    showWinMessage();
+                }
+                else
+                {
+                    lossNumber++;
+                    showLossMessage();
+                }
+
+            }
+            /*
+// When the player clicks the spin button the game kicks off
+            $("#spinButton").click(function () {
+                playerBet = $("div#betEntry>input").val();
+
+                if (playerMoney == 0)
+                {
+                    if (confirm("You ran out of Money! \nDo you want to play again?")) {
+                        resetAll();
+                        showPlayerStats();
+                    }
+                }
+                else if (playerBet > playerMoney) {
+                    alert("You don't have enough Money to place that bet.");
+                }
+                else if (playerBet < 0) {
+                    alert("All bets must be a positive $ amount.");
+                }
+                else if (playerBet <= playerMoney) {
+                    spinResult = Reels();
+                    fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+                    $("div#result>p").text(fruits);
+                    determineWinnings();
+                    turn++;
+                    showPlayerStats();
+                }
+                else {
+                    alert("Please enter a valid bet amount");
+                }
+
+            });
+             */
+        }
+
+
 
         public void updateCourt() {
+
+
+
+
+            /*
             if (racketIsMovingRight) {
                 racketPosition.x = racketPosition.x + 10;
             }
@@ -301,6 +547,8 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             }
+
+            */
         }
 
         public void drawCourt() {
